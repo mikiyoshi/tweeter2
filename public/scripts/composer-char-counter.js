7d8dc5c1f349c27f0693e8471e56e6d6b9e182f2
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-02-01 04:08:02 +0000

    add DOM character counter and composer-char-counter.js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  // --- our code goes here ---
+
+  const $countUpdate = $('#count-update');
+  const $tweetText = $('#tweet-text');
+
+  // text area
+  const $button = $('#update-button');
+  // submit
+
+  $tweetText.on('input', function () {
+    const tweetCount = $tweetText.val().length;
+    if (tweetCount >= 140) {
+      const $findCounter = $(this)
+        .siblings('.tweetCounter')
+        .find('#count-update')
+        .addClass('red')
+        .html(140 - tweetCount);
+    } else if (tweetCount < 140) {
+      const $findCounter = $(this)
+        .siblings('.tweetCounter')
+        .find('#count-update')
+        .removeClass('red')
+        .html(140 - tweetCount);
+    }
+    const $findCounter = $(this)
+      .siblings('.tweetCounter')
+      .find('#count-update')
+      .html(140 - tweetCount);
+  });
+});
